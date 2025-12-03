4f67cba4cb01abd9512513ae13502e3170b12e0b
Luca <hr.jeon28@gmail.com>
2025-12-03 19:37:49 +0900

    feat: Add design system components

---
+++ app/src/main/java/com/luca/myapplication/ui/theme/AppFonts.kt
@@ -0,0 +1,11 @@
+package com.luca.myapplication.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.luca.myapplication.R
+
+object AppFonts {
+    val Poppins = FontFamily(
+        Font(R.font.poppins_regular)
+    )
+}
